248e5dca72e8a461c00627b91d6fc2167f419641
John Coppola <jcoppola@insiten.com>
2020-09-04 21:03:00 -0400

    deals to player and dealer hands

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,19 @@
 	import type { IDrawData } from "./models/api/draw-data.interface";
 	import type { ICardData } from "./models/api/card-data.interface";
 	import Hand from "./Hand.svelte";
-import { onMount } from "svelte";
+	import { onMount } from "svelte";
 
+	interface IDealtCards {
+		player: ICard[];
+		dealer: ICard[];
+	}
+
+	let playing: boolean = false;
 	let deck: IDeck | undefined;
-	let cards: ICard[] = [];
-	$: cardsTotal = cards.reduce((total, card) => total + card.point, 0);
-	$: console.log(cardsTotal);
+	let playerCards: ICard[] = [];
+	let dealerCards: ICard[] = [];
+	$: playerTotal = getHandTotal(playerCards);
+	$: dealerTotal = getHandTotal(dealerCards);
 
 	onMount(async () => {
 		const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6");
@@ -23,17 +30,22 @@ import { onMount } from "svelte";
 	});
 
 	async function dealHand(): Promise<void> {
-		if (!deck) {
-			return;
-		}
-		const response = await fetch(`https://deckofcardsapi.com/api/deck/${deck.id}/draw/?count=2`);
+		playing = true;
+		const response = await fetch(`https://deckofcardsapi.com/api/deck/${deck.id}/draw/?count=4`);
 		const data: IDrawData = await response.json();
-		const newCards: ICard[] = data.cards.map(cardResponse => {
+		const newCards: IDealtCards = data.cards.reduce((accum, cardResponse, index) => {
 			const { image, value, suit, code }: ICardData = cardResponse;
 			const point = getCardPoint(value);
-			return { image, value, point, suit, code };
-		});
-		cards = [...cards, ...newCards];
+			const newCard = { image, value, point, suit, code };
+			if ((index + 1) % 2 === 0) {
+				accum.dealer.push(newCard);
+			} else {
+				accum.player.push(newCard);
+			}
+			return accum;
+		}, { player: [] as ICard[], dealer: [] as ICard[] });
+		playerCards = [...playerCards, ...newCards.player];
+		dealerCards = [...dealerCards, ...newCards.dealer];
 	}
 
 	function getCardPoint(value: FaceCard | string): number {
@@ -45,19 +57,32 @@ import { onMount } from "svelte";
 			return Number(value);
 		}
 	}
+
+	function getHandTotal(cards: ICard[]): number {
+		return cards.reduce((total, card) => total + card.point, 0);
+	}
 </script>
 
 <main class="app">
 	<h1>Blackjack</h1>
 	{#if deck}
-		<button on:click={dealHand}>
+		<button
+			disabled={playing}
+			on:click={dealHand}
+		>
 			Deal
 		</button>
 	{/if}
-	<Hand
-		cards={cards}
-		total={cardsTotal}
-	/>
+	<div class="table">
+		<Hand
+			cards={dealerCards}
+			total={dealerTotal}
+		/>
+		<Hand
+			cards={playerCards}
+			total={playerTotal}
+		/>
+	</div>
 </main>
 
 <style>
