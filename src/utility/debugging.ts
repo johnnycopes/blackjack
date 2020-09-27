49ff395485478d5ed932efd4c5b5927e5edde916
John Coppola <jcoppola@insiten.com>
2020-09-27 12:10:30 -0400

    writes createResponseData function to reduce repeated code

--- src/utility/debugging.ts
+++ src/utility/debugging.ts
@@ -2,6 +2,9 @@ import type { IDeckData } from "../models/api/deck-data.interface";
 import type { IDeck } from "../models/interfaces/deck.interface";
 import type { CardCode } from "../models/types/card-code.type";
 import { API_URL } from "../models/constants";
+import type { ICardData } from "../models/api/card-data.interface";
+import type { Suit } from "../models/types/suit.type";
+import type { CardValue } from "../models/types/card-value";
 
 export interface IOrderedDeckConfig {
 	player: CardCode[];
@@ -24,4 +27,46 @@ export async function fetchOrderedDeck(config: IOrderedDeckConfig): Promise<IDec
 		id: data.deck_id,
 		remaining: data.remaining,
 	};
+}
+
+export function createCardData(cardCode: CardCode): ICardData {
+	return {
+		code: cardCode,
+		suit: getSuit(cardCode),
+		value: getValue(cardCode),
+		image: `https://deckofcardsapi.com/static/img/${cardCode}.png`,
+		images: {
+			svg: `https://deckofcardsapi.com/static/img/${cardCode}.svg`,
+			png: `https://deckofcardsapi.com/static/img/${cardCode}.png`,
+		}
+	};
+}
+
+function getSuit(cardCode: CardCode): Suit {
+	const suitLetter = cardCode[1];
+	if (suitLetter === "S") {
+		return "SPADES";
+	} else if (suitLetter === "D") {
+		return "DIAMONDS";
+	} else if (suitLetter === "C") {
+		return "CLUBS";
+	} else if (suitLetter === "H") {
+		return "HEARTS";
+	}
+	throw new Error("Invalid card code");
+}
+
+function getValue(cardCode: CardCode): CardValue {
+	const value = cardCode[0];
+	if (value === "A") {
+		return "ACE";
+	} else if (value === "K") {
+		return "KING";
+	} else if (value === "Q") {
+		return "QUEEN";
+	} else if (value === "J") {
+		return "JACK";
+	} else {
+		return value.toString() as CardValue;
+	}
 }
