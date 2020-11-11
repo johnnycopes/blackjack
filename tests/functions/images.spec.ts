9bb598f01076b8802fa445035063c3df215560ce
John Coppola <jcoppola@insiten.com>
2020-11-11 15:20:35 -0500

    removes imageStrategyPreload test attempt and leaves explanatory note; updates project name in package.json

--- tests/functions/images.spec.ts
+++ tests/functions/images.spec.ts
@@ -1,19 +1,9 @@
-// import fetchMock from "jest-fetch-mock";
-import { IImage } from "../../src/models/interfaces/image.interface";
-import {
-	imageStrategyNone,
-	imageStrategyOnDemand,
-	// imageStrategyPreload
-} from "../../src/functions/images";
-	// import { preloadImage } from "../../src/functions/utility";
+import type { IImage } from "../../src/models/interfaces/image.interface";
+import { imageStrategyNone, imageStrategyOnDemand } from "../../src/functions/images";
 
 const images: IImage[] = [
 	{ name: "ACE_SPADES", src: "./assets/cards/ace_spades.png" }
 ];
-
-// beforeEach(() => {
-// 	fetchMock.resetMocks();
-// });
 
 describe("imageStrategyNone", () => {
 	it("returns an empty map", async () => {
@@ -31,33 +21,7 @@ describe("imageStrategyOnDemand", () => {
 	});
 });
 
-// describe("preloadImage", () => {
-// 	it("works", async () => {
-// 		const mockResponseData = {
-// 			url: "https://blackjack-testing-wip.surge.sh/assets/cards/ace_spades.png"
-// 		};
-// 		fetchMock.mockResponseOnce(JSON.stringify(mockResponseData));
-
-// 		const image = await preloadImage("./assets/cards/ace_spades.png");
-// 		expect(image).toEqual(1);
-// 	});
-// });
-
-// describe("imageStrategyPreload", () => {
-// 	const mockResponseData = {
-// 		url: "https://blackjack-testing-wip.surge.sh/assets/cards/ace_spades.png"
-// 	};
-// 	fetchMock.mockResponseOnce(JSON.stringify(mockResponseData));
-
-// 	it("returns a map of image names and their blob URLs", async () => {
-// 		const mockResponseData = {
-// 			url: "https://blackjack-testing-wip.surge.sh/assets/cards/ace_spades.png"
-// 		};
-// 		fetchMock.mockResponseOnce(JSON.stringify(mockResponseData));
-
-// 		const result = await imageStrategyPreload(images);
-// 		const expected = new Map();
-// 		expected.set("ACE_SPADES", "./assets/cards/ace_spades.png");
-// 		expect(result).toMatchObject(expected);
-// 	});
-// });
+/*
+	NOTE: there's no test for imageStrategyPreload because the URL object
+	is not supported in the js-dom library
+*/
